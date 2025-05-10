88ac1afb9871c1179475db14e12dc89f197b81db
heitorrosa <heitorolivrosa@gmail.com>
2025-05-10 15:31:53 -0300

    base files for the autoclicker

--- autoclicker.c
+++ autoclicker.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <windows.h>
 #include <winuser.h>
-#include <iostream>
-#include <thread>
+
 
 int main() {
+
+    int cps;
+    bool status = false, inventario, quebrarBlocos;
+
+
+    // Seletor de CPS
+    printf("Selecione o número de cliques por segundo (CPS):\n");
+    scanf("%d", &cps);
+
+    if(cps < 1) {
+        printf("CPS deve ser maior que 0.\n");
+        return 0;
+    }
+
+    printf("Pressione a tecla 'V' iniciar o AutoClicker.\n");
+
+    while(true) {
+        if (GetAsyncKeyState('V') & 0x8000 != 0) {
+            status = !status;
+            printf("AutoClicker %s!\n", status ? "Ativado" : "Desativado");
+        }
+    }
 }
