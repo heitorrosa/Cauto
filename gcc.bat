ff36b15ccce3d55396e55dc19f97a587f38170e2
heitorrosa <heitorolivrosa@gmail.com>
2025-07-07 02:44:27 -0300

    aasdasdasd

--- gcc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-gcc -o "C:\Windows\Temp\main" "main.c" -lm -lwinmm
-cmd /c start cmd /k "C:\Windows\Temp\main"
--- out/build/Cauto/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repositories/Cauto
-# Build directory: D:/Repositories/Cauto/out/build/Cauto
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
