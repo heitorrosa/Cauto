c58584cb4d5028af5b23585a19204001df6988fa
heitorrosa <heitorolivrosa@gmail.com>
2025-07-18 16:22:01 -0300

    compiler

---
+++ build/CMakeFiles/Cauto.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Repositories/Cauto/src/clicker/clicker.c" "CMakeFiles/Cauto.dir/src/clicker/clicker.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/clicker/clicker.c.obj.d"
+  "D:/Repositories/Cauto/src/main.c" "CMakeFiles/Cauto.dir/src/main.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/main.c.obj.d"
+  "D:/Repositories/Cauto/src/player/player.c" "CMakeFiles/Cauto.dir/src/player/player.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/player/player.c.obj.d"
+  "D:/Repositories/Cauto/src/recorder/recorder.c" "CMakeFiles/Cauto.dir/src/recorder/recorder.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/recorder/recorder.c.obj.d"
+  "D:/Repositories/Cauto/src/utils/hwid.c" "CMakeFiles/Cauto.dir/src/utils/hwid.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/utils/hwid.c.obj.d"
+  "D:/Repositories/Cauto/src/utils/utils.c" "CMakeFiles/Cauto.dir/src/utils/utils.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/utils/utils.c.obj.d"
+  "D:/Repositories/Cauto/src/websocket_example.c" "CMakeFiles/Cauto.dir/src/websocket_example.c.obj" "gcc" "CMakeFiles/Cauto.dir/src/websocket_example.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/external/libwebsockets-4.4.1/cmake_install.cmake
@@ -0,0 +1,95 @@
+# Install script for directory: D:/Repositories/Cauto/src/external/libwebsockets-4.4.1
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Cauto")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "D:/Program Files/MSYS2/mingw64/bin/objdump.exe")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "dev" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "D:/Repositories/Cauto/src/external/libwebsockets-4.4.1/include/libwebsockets")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "dev" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES
+    "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/include/libwebsockets.h"
+    "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/include/lws_config.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "dev" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/cmake" TYPE FILE FILES
+    "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/CMakeFiles/libwebsockets-config.cmake"
+    "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/libwebsockets-config-version.cmake"
+    "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/CMakeFiles/LwsCheckRequirements.cmake"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "dev" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/LibwebsocketsTargets.cmake")
+    file(DIFFERENT _cmake_export_file_changed FILES
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/LibwebsocketsTargets.cmake"
+         "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/CMakeFiles/Export/272ceadb8458515b2ae4b5630a6029cc/LibwebsocketsTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/LibwebsocketsTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/LibwebsocketsTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
+      endif()
+      unset(_cmake_old_config_files)
+    endif()
+    unset(_cmake_export_file_changed)
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/cmake" TYPE FILE FILES "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/CMakeFiles/Export/272ceadb8458515b2ae4b5630a6029cc/LibwebsocketsTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^()$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/cmake" TYPE FILE FILES "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/CMakeFiles/Export/272ceadb8458515b2ae4b5630a6029cc/LibwebsocketsTargets-noconfig.cmake")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/lib/cmake_install.cmake")
+  include("D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/plugins/cmake_install.cmake")
+  include("D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/lwsws/cmake_install.cmake")
+
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
