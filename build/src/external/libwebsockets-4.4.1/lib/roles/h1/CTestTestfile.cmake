603840cfec36a49bd0265bf06bb54df3da620d79
heitorrosa <heitorolivrosa@gmail.com>
2025-07-20 23:10:08 -0300

    idk

--- build/src/external/libwebsockets-4.4.1/lib/roles/h1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repositories/Cauto/src/external/libwebsockets-4.4.1/lib/roles/h1
-# Build directory: D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/lib/roles/h1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
