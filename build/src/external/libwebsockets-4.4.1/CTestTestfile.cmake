1878e152690679aeb6f82db96c59cd25a2608556
heitorrosa <heitorolivrosa@gmail.com>
2025-08-16 21:41:24 -0300

    gitignore the build folder

--- build/src/external/libwebsockets-4.4.1/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repositories/Cauto/src/external/libwebsockets-4.4.1
-# Build directory: D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("plugins")
-subdirs("lwsws")
--- build/src/external/libwebsockets-4.4.1/lib/event-libs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repositories/Cauto/src/external/libwebsockets-4.4.1/lib/event-libs
-# Build directory: D:/Repositories/Cauto/build/src/external/libwebsockets-4.4.1/lib/event-libs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("poll")
